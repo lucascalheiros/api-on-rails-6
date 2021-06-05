331dfa15a5d90689a96917752a828f83d11089ce
Lucas <lucascalheirosjllc@gmail.com>
2021-06-05 15:03:48 -0300

    creating user, migration, and adding unit tests

---
+++ test/models/user_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test 'user email invalid' do
+    user = User.new(email: 'test', password_digest: '000')
+    assert_not user.valid?
+  end
+  test 'user email already taken' do
+    some_user = users(:one)
+    user = User.new(email: some_user.email, password_digest: '000')
+    assert_not user.valid?
+  end
+  test 'user valid' do
+    user = User.new(email: 'testValid@test.com', password_digest: '000')
+    assert user.valid?
+  end
+end

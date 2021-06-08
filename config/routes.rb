c670c542a617e5a51a55843ecf3f4f3c725d77c2
Lucas <lucascalheirosjllc@gmail.com>
2021-06-07 22:52:00 -0300

    finishing products read

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: %i[show create update destroy]
+      resources :products, only: %i[show index]
     end
   end
 end

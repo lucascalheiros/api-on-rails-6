214f4584c14a9775d443a3647838fada183d7a3a
Lucas <lucascalheirosjllc@gmail.com>
2021-06-09 22:24:18 -0300

    adding filter scopes

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -6,4 +6,25 @@ class ProductTest < ActiveSupport::TestCase
     product.price = -1
     assert_not product.valid?
   end
+
+  test 'should filte product by name' do
+    assert_equal 2, Product.filter_by_title('tv').count
+  end
+
+  test 'should filter products by name and sort them' do
+    assert_equal [products(:three), products(:two)], Product.filter_by_title('tv').sort
+  end
+
+  test 'should filter products by price above and sort them' do
+    assert_equal [products(:three), products(:two)], Product.above_or_equal_to_price(9).sort
+  end
+
+  test 'should filter products by price below and sort them' do
+    assert_equal [products(:two), products(:one)], Product.below_or_equal_to_price(10).sort
+  end
+
+  test 'should sort product by most recent' do
+    products(:two).touch
+    assert_equal [products(:three), products(:one), products(:two)], Product.recent.to_a
+  end
 end

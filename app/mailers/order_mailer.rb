f1b544e47e2c00e8c965b9e53f13c08f78d85052
Lucas <lucascalheirosjllc@gmail.com>
2021-07-04 18:35:10 -0300

    adding confirmation mail

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,13 +1,8 @@
 class OrderMailer < ApplicationMailer
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.order_mailer.send_confirmation.subject
-  #
-  def send_confirmation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  default from: 'from@example.com'
+  def send_confirmation(order)
+    @order = order
+    @user = @order.user
+    mail to: @user.email, subject: 'Order confirmation'
   end
 end

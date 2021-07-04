f6da32abf921aaba295ce01ecc1c1223aa935fed
Lucas <lucascalheirosjllc@gmail.com>
2021-07-03 22:43:48 -0300

    generating orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_011418) do
+ActiveRecord::Schema.define(version: 2021_07_04_013516) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -29,5 +37,6 @@ ActiveRecord::Schema.define(version: 2021_06_08_011418) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end
